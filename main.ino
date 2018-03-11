37906338c818b2deaa6fc07b3ecc39922766e768
atrantan <atrantan@gmail.com>
2018-03-11 19:09:53 +0100

    Ajout de squelette de code vide

---
+++ main.ino
@@ -0,0 +1,14 @@
+//  Copyright (c) 2018 Antoine Tran Tan
+//
+
+#include "Arduino.h"
+
+// the setup function runs once when you press reset or power the board
+void setup()
+{
+}
+
+// the loop function runs over and over again forever
+void loop()
+{
+}
