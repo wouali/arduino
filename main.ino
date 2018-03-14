bb5491252498d76e0b8dd2301bafe9802c949447
wouali <wissamouali773@gmail.com>
2018-03-14 15:22:33 +0100

    main.ino

--- main.ino
+++ main.ino
@@ -6,9 +6,14 @@
 // the setup function runs once when you press reset or power the board
 void setup()
 {
+    pinMode(13, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop()
 {
+    digitalWrite(13, HIGH);
+    delay(2000);
+    digitalWrite(13, LOW);
+    delay(2000);
 }
