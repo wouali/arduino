b10a98655fb2f08760998bfbcec189777fcb2dbc
wouali <wissamouali773@gmail.com>
2018-03-14 16:37:21 +0100

    super

--- main.ino
+++ main.ino
@@ -12,8 +12,10 @@ void setup()
 // the loop function runs over and over again forever
 void loop()
 {
+static int potPosition;
+potPosition = analogRead(A0);
     digitalWrite(13, HIGH);
-    delay(2000);
+    delay(potPosition);
     digitalWrite(13, LOW);
-    delay(2000);
+    delay(potPosition);
 }
